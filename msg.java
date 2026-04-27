a740be5ce1f22bd064fece97df770bbb6a7142ab
Mahesh <mpmahesh2342@gmail.com>
2026-04-27 15:38:31 +0530

    modified commit

--- msg.java
+++ msg.java
@@ -2,5 +2,7 @@ public class msg {
 	public static void main(string[] args){
 		int a=10; int b=2;
 		system.out.println("Addition is--:"+(a=b));
+		system.out.println("difference is--:"+(a-b));
+		system.out.println("product is--:"+(a*b));
 	}
 }
